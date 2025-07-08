a1f89a4ab97675304857924a78c86415b8a7f74d
yaitaimo <yaitaimo@gmail.com>
2025-07-08 12:23:35 +0900

    不要な設定を削除

--- qmk_firmware/keyboards/keyball/keyball44/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/config.h
@@ -88,9 +88,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_TERM 200
 
-// オートマウスレイヤーを有効化
-#define AUTO_MOUSE_LAYER_ENABLE
-
 // オートマウスレイヤーのタイムアウト時間
 #define AUTO_MOUSE_LAYER_TIMEOUT 150
 
